0029ca2cf0bfd872eea0a26ebe3f4ada003b5814
Alberto <alberto.mecca@cern.ch>
2023-10-03 18:42:05 +0200

    Added compilation errors to summary

--- scripts/summary-log.sh
+++ scripts/summary-log.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-errors=( "Error" "LaTeX" "Package" )
-patterns=( "LaTeX(?= Error)" "LaTeX(?= Warning)" "(?<=Package )[^ ]+(?= Warning)" )
+errors=( "Error" "LaTeX" "Package" "Compilation" )
+patterns=( "LaTeX(?= Error)" "LaTeX(?= Warning)" "(?<=Package )[^ ]+(?= Warning)" "^l.[\d]+")
+options=( "" "" "" "-C1" )
 logfile=${1:-output/AN-21-207_temp.log}
 
 [ -f $logfile ] || { echo "ERROR file not found:" $logfile >&2 ; exit 1; }
 
 for i in "${!errors[@]}" ; do
     printf "%s: %d\n" "${errors[i]}" $(grep -cP "${patterns[i]}" $logfile)
-    grep --color=always -P "${patterns[i]}" $logfile | sed "s/^/\t/g"
+    grep --color=always -P ${options[i]} "${patterns[i]}" $logfile | sed "s/^/\t/g"
 done
