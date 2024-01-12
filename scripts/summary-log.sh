7743e0c352c5d01e7b0bfa979d4181a78283ef5d
Alberto Mecca <alberto.mecca@cern.ch>
2024-01-12 17:08:35 +0100

    Summary includes underfull/overfull warnings

--- scripts/summary-log.sh
+++ scripts/summary-log.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-errors=(  "Compilation" "Error" "LaTeX" "Package" )
-patterns=( "^l.[\d]+" "LaTeX(?= Error)" "LaTeX( [^ ]+)*(?= Warning)" "(?<=Package )[^ ]+(?= Warning)" )
-options=( "-A3" "" "" "" "" )
+errors=(  "Compilation" "Error" "LaTeX" "Package" "Overfull hbox" "Underfull vbox" )
+patterns=( "^l.[\d]+" "LaTeX(?= Error)" "LaTeX( [^ ]+)*(?= Warning)" "(?<=Package )[^ ]+(?= Warning)" "Overfull \\\\hbox" "Underfull \\\\vbox" )
+options=( "-A3" "" "" "" "-q" "-q" )
 logfile=${1:-output/AN-21-207_temp.log}
 
 [ -f $logfile ] || { echo "ERROR file not found:" $logfile >&2 ; exit 1; }
